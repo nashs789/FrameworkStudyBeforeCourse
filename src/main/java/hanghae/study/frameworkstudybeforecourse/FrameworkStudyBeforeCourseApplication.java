2d5bae81cf2d489c38d7c218700b794a1353c5f1
nashs789 <nashs789@naver.com>
2024-07-29 23:45:12 +0900

    feat: init

---
+++ src/main/java/hanghae/study/frameworkstudybeforecourse/FrameworkStudyBeforeCourseApplication.java
@@ -0,0 +1,13 @@
+package hanghae.study.frameworkstudybeforecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkStudyBeforeCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkStudyBeforeCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hanghae/study/frameworkstudybeforecourse/FrameworkStudyBeforeCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package hanghae.study.frameworkstudybeforecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkStudyBeforeCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
